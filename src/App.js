f83b2b0bdbdfe25bb5a55715746c0706a3405d3d
amir <mashhadifarahani1@gmail.com>
2023-10-14 20:30:39 +0330

    adding custom slider

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
 import Menu from './components/Menu/Menu';
 import Footer from './components/Footer/Footer';
-
+import React from 'react';
+import Slider from './components/Slider/Slider';
+import first from './assets/1.png'
+import second from './assets/header-bg (2).jpg'
 function App() {
+  const slides = [
+    { url: "image-1.jpg", title: "beach" },
+    { url: "image-2.jpg", title: "boat" },
+    { url: "image-3.jpg", title: "forest" },
+    { url: "image-4.jpg", title: "city" },
+    { url: "image-5.jpg", title: "italy" },
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
   return (
     <div className="App">
       <div className='header'>
@@ -13,6 +28,9 @@ function App() {
         <Header/>
       </div>
       <Menu/>
+      <div style={containerStyles}>
+        <Slider slides={slides} parentWidth={500} />
+      </div>
       <Footer/>
     </div>
   );
