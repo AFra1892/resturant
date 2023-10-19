e860e2405e30fc2dca532441c871078a95fc0b16
amir <mashhadifarahani1@gmail.com>
2023-10-19 15:30:36 +0330

    continue responsive best deal and adding img to slider

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 import Slider from '../components/Slider/Slider';
 import Drink from '../components/Drink/Drink';
 import BestDeal from '../components/bestDeal/BestDeal';
-
+import people from '../assets/people.png'
 
 function Landing() {
   const slides = [
@@ -35,7 +35,11 @@ function Landing() {
         <div className='slider-container_content'>
           <h4><i class='bx bxl-instagram-alt bx-sm'></i> Instagram</h4>
           <h2>Photo Gallery</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          <div className="gpt3__header-content__people">
+            <img src={people} />
+            <p>1,600 people requested access a visit in last 24 hours</p>
+          </div>
           <button>View More</button>
         </div>
         <div className='slider-container' >
