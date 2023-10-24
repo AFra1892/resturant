3b4540f7b61b2849b0822c903ea2c5ad5280aa25
amir <mashhadifarahani1@gmail.com>
2023-10-24 21:49:20 +0330

    responsive about page

--- src/components/aboutCard/About.jsx
+++ src/components/aboutCard/About.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import Navbar from '../navbar/Navbar'
 import AboutCard from './AboutCard'
-
+import './about.css'
 const About = () => {
-    const page_style ={
-      textAlign:'center'
-    }
-    const styles={
-      fontSize: "54px",
-      fontWeight: "700",
-      lineHeight: "56px",
-      marginBottom:"50px"
-    }
-    const card_style ={
-      display:"grid",
-      gridTemplateColumns:"1fr 1fr",
-      gap:"50px"
-    }
+   
   return (
-        <div style={page_style} className='restirant__about section__padding'>
-            <h2 style={styles}>About Our Foods</h2>
-            <div style={card_style} className='resturant__about-cards'>
+        <div  className='restirant__about section__padding'>
+            <h2>About Our Foods</h2>
+            <div className='resturant__about-cards'>
               <AboutCard img='images/about1.jpg' heading='Commitment to Quality' dec="We're dedicated to improving the way we prepare our quality food and the ingredients that go into it"/>
               <AboutCard img='images/about2.jpg' heading='Commitment to Quality' dec="We're dedicated to improving the way we prepare our quality food and the ingredients that go into it"/>
               <AboutCard img='images/about3.jpg' heading='Commitment to Quality' dec="We're dedicated to improving the way we prepare our quality food and the ingredients that go into it"/>
