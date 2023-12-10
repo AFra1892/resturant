852d0227e8cdddbb63f7478555131fc6154a4694
amir <mashhadifarahani1@gmail.com>
2023-12-10 22:26:01 +0330

    log out feature

--- Frontend/src/pages/DashTest.jsx
+++ Frontend/src/pages/DashTest.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import jwt from 'jsonwebtoken'
-
+import Navbar from '../components/navbar/Navbar'
+import { IoMdExit } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 const DashTest = () => {
-    const token = localStorage.getItem('token')
-    const t = token.split(' ')[1]
-    const decoded = jwt.verify(t,'myfirstprojectworkingwithjwt10801')
-    console.log(decoded);
+    const [logedInUser,setLogedInUser] = useState({})
+    const navigate = useNavigate();
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+        const t = token.split(' ')[1]
+        const decoded = jwt.verify(t,'myfirstprojectworkingwithjwt10801')
+        setLogedInUser(decoded)
+        console.log(decoded);
+
+    },[])
+    const styles = {
+        backgroundImage: `url(${logedInUser.img})`,
+        backgroundSize: 'cover'
+    }
+    function clickHandeler(){
+		alert('token was deleted')
+		localStorage.removeItem('token')
+		
+		navigate('/')
+	}
   return (
-    <div>{decoded.name}</div>
+    <>
+    <div className='h-screen bg-red-400' style={styles}>
+        <Navbar/>
+        <IoMdExit onClick={clickHandeler} />
+        {logedInUser.name}
+    
+    </div>
+    
+    </>
   )
 }
 
