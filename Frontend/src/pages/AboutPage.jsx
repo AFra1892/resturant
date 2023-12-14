8d8c942f875e06e63d136ef4158c2029e386cf71
amir <mashhadifarahani1@gmail.com>
2023-12-14 23:51:02 +0330

    removing About Component

--- Frontend/src/pages/AboutPage.jsx
+++ Frontend/src/pages/AboutPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import About from '../components/aboutCard/About'
+import AboutCard from '../components/aboutCard/AboutCard'
 const AboutPage = () => {
   return (
     <>
-        <Navbar/>
-        <About/>
+        <div  className='restirant__about section__padding'>
+            <h2>About Our Foods</h2>
+            <div className='resturant__about-cards'>
+              <AboutCard img='images/about1.jpg' heading='Commitment to Quality' dec="We're dedicated to improving the way we prepare our quality food and the ingredients that go into it"/>
+              <AboutCard img='images/about2.jpg' heading='Commitment to Quality' dec="We're dedicated to improving the way we prepare our quality food and the ingredients that go into it"/>
+              <AboutCard img='images/about3.jpg' heading='Commitment to Quality' dec="We're dedicated to improving the way we prepare our quality food and the ingredients that go into it"/>
+              <AboutCard img='images/about4.jpg' heading='Commitment to Quality' dec="We're dedicated to improving the way we prepare our quality food and the ingredients that go into it"/>
+            </div>
+        </div>
     </>
   )
 }
