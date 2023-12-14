f344d353c88ba72aadd4b9747d054f84018e2784
amir <mashhadifarahani1@gmail.com>
2023-12-14 23:35:33 +0330

    imporoving sign up input

--- Frontend/src/pages/auth/SignUp.jsx
+++ Frontend/src/pages/auth/SignUp.jsx
@@ -41,28 +41,77 @@ const SignUp = () => {
             <h1 className='text-6xl'> Welcome !</h1>
             <h3 className='text-green-700 py-4 '>Give us some information to get better exprience</h3>
             <div className=' flex flex-col gap-y-6'>
-
+              <div>
+                
+            <label for="input1" class="text-sm text-gray-700 block mb-1 font-medium">
+                Username
+              </label>
             <input
                 type='text'
                 value={username}
                 onChange={(e) => setUserName(e.target.value)}
-                className='border-2 border-green-600 px-4 py-2 w-full'
+                class="w-full bg-green-50 border border-green-400 rounded py-1 px-3 block focus:ring-green-500 focus:border-green-500 text-gray-700"
                 placeholder='Name'
                 />
-            <input
+                </div>
+
+                <div>
+                  
+                <label for="input1" class="text-sm text-gray-700 block mb-1 font-medium">
+                Email
+               </label>
+                <input
                 type='email'
                 value={useremail}
                 onChange={(e) => setUserEmail(e.target.value)}
-                className='border-2 border-green-600 px-4 py-2 w-full'
+                class="w-full bg-green-50 border border-green-400 rounded py-1 px-3 block focus:ring-green-500 focus:border-green-500 text-gray-700"
                 placeholder='Email'
                 />
-            <input
+                </div>
+
+
+
+
+
+
+            <div>
+              <label for="input1" class="text-sm text-gray-700 block mb-1 font-medium">
+                Password
+              </label>
+              <input
+                type="password"
+                name="input1"
+                id="input1"
+                value={userpass}
+                onChange={(e) => setUserPass(e.target.value)}
+                class="w-full bg-green-50 border border-green-400 rounded py-1 px-3 block focus:ring-green-500 focus:border-green-500 text-gray-700"
+                placeholder="Enter your password"
+              />
+              <div class="flex items-center mt-1 text-green-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <p class="ml-1 text-xs">{userpass.length > 5 ?"Secure Password":""}</p>
+              </div>
+            </div>
+
+            
+            {/* <input
                 type='password'
                 value={userpass}
                 onChange={(e) => setUserPass(e.target.value)}
                 className='border-2 border-green-600 px-4 py-2 w-full '
                 placeholder='Password'
-                />
+                /> */}
             <div className='flex gap-x-3'>
 
                 <input
