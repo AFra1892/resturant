218bc0f2087ac5ffceb1aa5c581497be29af790c
amir <mashhadifarahani1@gmail.com>
2023-12-20 16:13:16 +0330

    re design sign in page responsive

--- Frontend/src/pages/auth/SignUp.jsx
+++ Frontend/src/pages/auth/SignUp.jsx
@@ -35,101 +35,90 @@ const SignUp = () => {
       };
 
   return (
-    <div className='flex h-screen '>
-        <div className='w-3/6 bg-green-300'></div>
-        <div className=' w-3/6 px-20 pt-20'>
-            <h1 className='text-6xl'> Welcome !</h1>
-            <h3 className='text-green-700 py-4 '>Give us some information to get better exprience</h3>
-            <div className=' flex flex-col gap-y-6'>
-              <div>
-                
-            <label for="input1" class="text-sm text-gray-700 block mb-1 font-medium">
-                Username
-              </label>
-            <input
-                type='text'
-                value={username}
-                onChange={(e) => setUserName(e.target.value)}
-                class="w-full bg-green-50 border border-green-400 rounded py-1 px-3 block focus:ring-green-500 focus:border-green-500 text-gray-700"
-                placeholder='Name'
-                />
-                </div>
+    <section class="flex flex-col md:flex-row h-screen items-center">
 
-                <div>
-                  
-                <label for="input1" class="text-sm text-gray-700 block mb-1 font-medium">
-                Email
-               </label>
-                <input
-                type='email'
-                value={useremail}
-                onChange={(e) => setUserEmail(e.target.value)}
-                class="w-full bg-green-50 border border-green-400 rounded py-1 px-3 block focus:ring-green-500 focus:border-green-500 text-gray-700"
-                placeholder='Email'
-                />
-                </div>
+  
+
+  <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+        flex items-center justify-center">
+
+    <div class="w-full h-100">
 
 
+      <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
 
-
-
-
-            <div>
-              <label for="input1" class="text-sm text-gray-700 block mb-1 font-medium">
-                Password
-              </label>
-              <input
-                type="password"
-                name="input1"
-                id="input1"
-                value={userpass}
-                onChange={(e) => setUserPass(e.target.value)}
-                class="w-full bg-green-50 border border-green-400 rounded py-1 px-3 block focus:ring-green-500 focus:border-green-500 text-gray-700"
-                placeholder="Enter your password"
-              />
-              <div class="flex items-center mt-1 text-green-700">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <p class="ml-1 text-xs">{userpass.length > 5 ?"Secure Password":""}</p>
-              </div>
-            </div>
-
-            
-            {/* <input
-                type='password'
-                value={userpass}
-                onChange={(e) => setUserPass(e.target.value)}
-                className='border-2 border-green-600 px-4 py-2 w-full '
-                placeholder='Password'
-                /> */}
-            <div className='flex gap-x-3'>
-
-                <input
-                    type='checkbox'
-                    id='terms'
-                    value={terms}
-                    onChange={(e) => setTerms(e.target.value)}
-                />
-                <label for='terms'>By creating an account you agree to the Terms of use</label>
-            </div>
-            </div>
-            <button className='mt-3 bg-green-600 w-full h-10' onClick={handleSaveUser}>Create Account</button>
-            <p>Already have an acoount? <a href='/signin' className='text-green-600'>Log in</a></p>
-
-
+      <form class="mt-6" action="#" method="POST" >
+      <div>
+          <label class="block text-gray-700">User Name</label>
+          <input
+              type="email" 
+              value={username}
+              onChange={(e) => setUserName(e.target.value)}
+              name="" 
+              id="" 
+              placeholder="Enter User Name" 
+              class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" 
+              autofocus autocomplete required />
         </div>
+
+      
+        <div className='mt-4'>
+          <label class="block text-gray-700">Email Address</label>
+          <input
+              type="email" 
+              value={useremail}
+              onChange={(e) => setUserEmail(e.target.value)}
+              name="" 
+              id="" 
+              placeholder="Enter Email Address" 
+              class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" 
+              autofocus autocomplete required />
+        </div>
+
+        <div class="mt-4">
+          <label class="block text-gray-700">Password</label>
+          <input 
+              type="password"
+              value={userpass}
+              onChange={(e) => setUserPass(e.target.value)} 
+              name="" 
+              id="" 
+              placeholder="Enter Password" 
+              minlength="4" 
+              class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" 
+              required/>
+        </div>
+
+        <div class="text-right mt-2">
+          <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
+        </div>
+
+        <button type="submit" class="w-full block bg-yellow-500 hover:bg-yellow-400 focus:bg-yellow-400 text-white font-semibold rounded-lg
+              px-4 py-3 mt-6" onClick={handleSaveUser}>Create Account</button>
+      </form>
+
+      <hr class="my-6 border-gray-300 w-full"/>
+
+      <button type="submit" class="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+            <div class="flex items-center justify-center">
+            <span class="ml-4">
+            Sign in
+            with
+            Google</span>
+            </div>
+          </button>
+
+      <p class="mt-8">Already have an acoount? <a href="/signin" class="text-yellow-500 hover:text-yellow-600 font-semibold">Sign in</a></p>
+
+
     </div>
+  </div>
+  <div class="bg-red-500 hidden lg:block w-full md:w-1/2 xl:w-1/3 h-screen">
+    {/* <img src="https://source.unsplash.com/random" alt="" class="w-full h-full object-cover"/> */}
+  </div>
+</section>
   )
 }
 
 export default SignUp
+
